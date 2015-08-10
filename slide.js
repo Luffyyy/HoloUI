e26b7f57f95a5b27dcfea4097faff64ad4e952d9
Daniel.R <danielandro11@gmail.com>
2015-08-10 19:46:21 +0300

    Update slide.js

--- slide.js
+++ slide.js
@@ -2,7 +2,7 @@ var imageCount = 1;
 var total = 6;
 
 function slider(x) {
-	var image = document.getElementById('image');
+	var image = document.getElementById('slide-img');
 	imageCount = imageCount + x;
 	if(imageCount > total){imageCount = 1;}
 	if(imageCount < 1){imageCount = total;}	

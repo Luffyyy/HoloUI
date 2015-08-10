8fbdb508d8a553c2b6616fc35f72d06d42432baa
Daniel.R <danielandro11@gmail.com>
2015-08-10 19:26:20 +0300

    Update slide.js

--- slide.js
+++ slide.js
@@ -4,5 +4,5 @@ var tot_imgs = 5;
 function slider(x){
     var Image = document.getElementById('screen');
     img_count = img_count + x;
-    Image.src = "img/screen"+ img_count +".png";
+    Image.src = "img/screen1"+ img_count +".png";
 }

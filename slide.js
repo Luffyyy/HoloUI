7095d060deaffdb8cec6d14fc3dbaadee3dcd563
Daniel.R <danielandro11@gmail.com>
2015-08-10 20:26:02 +0300

    Update slide.js

--- slide.js
+++ slide.js
@@ -4,11 +4,11 @@ var tot_imgs = 3;
 function slider(x) {
 	var image = document.getElementById('slide-img');
 	imgcount = imgcount + x;
-	if(imgcount > total){
+	if(imgcount > tot_imgs){
             imgcount = 1;
 	}
 	if(imgcount < 1){
-	    imgcount = total;
+	    imgcount = tot_imgs;
 	}
   	image.src = "img/img"+ imageCount +".jpg";
 }

d8f996edaae95c70824a3d3f7eb00761341560c6
Daniel.R <danielandro11@gmail.com>
2015-08-10 19:02:53 +0300

    Update slide.js

--- slide.js
+++ slide.js
@@ -2,7 +2,7 @@ var imgnum = 1;
 var tot_imgs = 5;
 
 function slider(x){
-	var Image = Document.getElementById()('screen');
+	var image = document.getElementById('screen');;
 	imgnum = imgnum + x;
 	if(imgnum > tot_imgs){
     imagecount = 1;
@@ -11,5 +11,4 @@ function slider(x){
     imagecount = tot_imgs;
     }
 	Image.src = "img/screen" + imgnum + ".png";
-
 }

482428ecd02bb1bd0ea57949e8b2d5d55d78c4c3
Daniel.R <danielandro11@gmail.com>
2015-08-10 18:42:55 +0300

    Create slide.js

---
+++ slide.js
@@ -0,0 +1,15 @@
+var imgnum = 1;
+var tot_imgs 5;
+
+function slider(x){
+	var Image = document.getElemetsById('screen');
+	imgnum = imgnum + x;
+	if(imgnum > tot_imgs){
+    imagecount = 1;
+    }
+    if(imgnum < tot_imgs){
+    imagecount = tot_imgs;
+    }
+	Image.src = "img/screen" + imgnum + ".png";
+
+}

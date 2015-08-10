b9cb1d2b72b575c9a9eefb26dccc492c53acb631
Daniel.R <danielandro11@gmail.com>
2015-08-10 19:15:35 +0300

    Update slide.js

--- slide.js
+++ slide.js
@@ -2,13 +2,13 @@ var imgnum = 1;
 var tot_imgs = 5;
 
 function slider(x){
-	var image = document.getElementById('screen');
-	imgnum = imgnum + x;
-	if(imgnum > tot_imgs){
-    imagecount = 1;
+    var Image = document.getElementById('screen');
+    imgnum = imgnum + x;
+    if(imgnum > tot_imgs){
+    imgnum = 1;
     }
     if(imgnum < tot_imgs){
-    imagecount = tot_imgs;
+    imgnum = tot_imgs;
     }
-	Image.src = "img/screen" + imgnum + ".png";
+    Image.src = "img/screen" + imgnum + ".png";
 }

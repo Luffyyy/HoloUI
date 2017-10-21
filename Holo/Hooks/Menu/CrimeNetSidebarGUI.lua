71d50b856e381219cdb7f03de7a857804b6339ac
bamq47 <bamq@users.noreply.github.com>
2017-10-21 16:53:58 -0500

    Sidebar GUI text highlight

--- Holo/Hooks/Menu/CrimeNetSidebarGUI.lua
+++ Holo/Hooks/Menu/CrimeNetSidebarGUI.lua
@@ -3,6 +3,7 @@ if Holo.Options:GetValue("Menu") then
 
 Holo:Post(CrimeNetSidebarItem, "init", function(self, sidebar, panel, parameters)
 	self:set_color(Holo:GetColor("TextColors/Menu"))
+	self:set_highlight_color(Holo:GetColor("TextColors/Marker"))
 end)
 
 end
